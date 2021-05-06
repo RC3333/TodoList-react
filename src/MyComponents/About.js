81bb1c8647fe9a7f7b92eb787b12cc708d4ed683
Rinkesh <chaudharirinkesh31@gmail.com>
2021-05-06 19:10:28 +0530

    Todo-List Project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is my about MyComponents
+            <p>qwertyuioasdfghjkl
+                asdfghjkl;
+                asdfghjklzxcvbnm
+                sdfghjkl\wertyuio
+            </p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+export const Footer = () => {
+
+    return (
+        <footer className='bg-dark text-light py-3' >
+            <p className="text-center">Copyright &copy; MyTodoList.com</p>
+            
+        </footer>
+    )
+}
